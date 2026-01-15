f2c349a3adeeb6a8ec5c4c2fb0ea783927de1700
tranquillo_manvith <manvithpoojary095@gmail.com>
2026-01-15 19:17:56 +0530

    fix: round1 login is strict

--- src/app/api/round1/login/route.ts
+++ src/app/api/round1/login/route.ts
@@ -49,11 +49,21 @@ export async function POST(req: NextRequest) {
       { $sample: { size: questionsCount } },
     ]);
 
-    const round1player = await Round1Player.findOneAndUpdate(
-      { teamId: team._id },
-      { startTime: new Date(), score: null, timeTaken: null },
-      { upsert: true, new: true }
-    );
+    const existingPlayer = await Round1Player.findOne({ teamId: team._id });
+
+    if (existingPlayer) {
+      return NextResponse.json(
+        { error: "Round 1 already started. Please contact event organizer" },
+        { status: 403 }
+      );
+    }
+
+    const round1player = await Round1Player.create({
+      teamId: team._id,
+      startTime: new Date(),
+      score: null,
+      timeTaken: null,
+    });
 
     const endTime = round1player.startTime.getTime() + ROUND1DURATION * 1000;
 
