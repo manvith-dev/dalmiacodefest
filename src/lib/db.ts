24cb4221153b47c7987b180c15303ff5a596af36
tranquillo_manvith <manvithpoojary095@gmail.com>
2026-01-19 19:43:12 +0530

    update: MongoDB connection cache

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,20 +3,31 @@ import mongoose from "mongoose";
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
-  throw new Error(
-    "Please define the MONGODB_URI environment variable inside .env.local"
-  );
+  throw new Error("MONGODB_URI is not defined");
 }
 
-let isConnected: boolean = false;
+type Cached = {
+  conn: typeof mongoose | null;
+  promise: Promise<typeof mongoose> | null;
+};
+
+const globalWithMongoose = global as typeof global & {
+  mongoose: Cached;
+};
+
+if (!globalWithMongoose.mongoose) {
+  globalWithMongoose.mongoose = { conn: null, promise: null };
+}
 
 export default async function connectDB() {
-  if (isConnected) return;
-  try {
-    await mongoose.connect(MONGODB_URI);
-    isConnected = true;
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    throw error;
+  const cached = globalWithMongoose.mongoose;
+
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
   }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
