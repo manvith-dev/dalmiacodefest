50db4cee04c8ac78162d0496067253803d71fc81
tranquillo_manvith <manvithpoojary095@gmail.com>
2026-01-30 19:42:03 +0530

    registration fields validations are stronger

--- src/features/auth/services/validations.ts
+++ src/features/auth/services/validations.ts
@@ -27,16 +27,36 @@ function isEqualCheck(values: RegisterFormValues) {
 
 function regexCheck(values: RegisterFormValues) {
   let errors: string[] = [];
+
+  //Regex
+  const teamNameRegex = /^(?=.*[A-Za-z])[A-Za-z0-9 _-]{3,20}$/;
+  const nameRegex = /^[A-Za-z ]{3,}$/;
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const phoneRegex = /^\d{10}$/;
-  if (!emailRegex.test(values.p1email))
-    errors.push(" Player 1 Email is invalid.");
-  if (!emailRegex.test(values.p2email))
-    errors.push(" Player 2 Email is invalid.");
-  if (!phoneRegex.test(values.p1phone))
-    errors.push(" Player 1 Phone is invalid.");
-  if (!phoneRegex.test(values.p2phone))
-    errors.push(" Player 2 Phone is invalid.");
+
+  //Regex Tests
+
+  //Team name check
+  if (!teamNameRegex.test(values.teamName.trim()))
+    errors.push("Team Name is invalid.");
+
+  //Names Check
+  if (!nameRegex.test(values.p1name.trim()))
+    errors.push("Player 1 Name is invalid.");
+  if (!nameRegex.test(values.p2name.trim()))
+    errors.push("Player 2 Name is invalid.");
+
+  //Email Checks
+  if (values.p1email.length < 6 || !emailRegex.test(values.p1email))
+    errors.push("Player 1 Email is invalid.");
+  if (values.p2email.length < 6 || !emailRegex.test(values.p2email))
+    errors.push("Player 2 Email is invalid.");
+
+  //Phone checks
+  if (!phoneRegex.test(values.p1phone.trim()))
+    errors.push("Player 1 Phone is invalid.");
+  if (!phoneRegex.test(values.p2phone.trim()))
+    errors.push("Player 2 Phone is invalid.");
 
   return errors;
 }
